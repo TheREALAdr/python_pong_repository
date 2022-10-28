171eb9777944aa578c7cced579892e20d1b62199
TheREALAdr <95942707+TheREALAdr@users.noreply.github.com>
2022-10-27 19:57:20 -0500

    Update paddle.py

--- paddle.py
+++ paddle.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
 
+
 class Paddle(Turtle):
     def __init__(self, position):
         super().__init__()
@@ -9,14 +10,22 @@ class Paddle(Turtle):
         self.shapesize(stretch_wid=1, stretch_len=5)
         self.penup()
         self.goto(position)
-   
 
-
-     
-    def go_down(self):
-        new_y = self.ycor() - 10
-        self.goto(self.xcor(), new_y)
-       
     def go_up(self):
-        new_y = self.ycor() + 10
-        self.goto(self.xcor(), new_y)
+        new_coords = (self.xcor(), self.ycor() + 20)
+        if self.ycor() >= 240:
+            new_coords = (self.xcor(), self.ycor())
+        self.goto(new_coords)
+
+    def go_down(self):
+        new_coords = (self.xcor(), self.ycor() - 20)
+        if self.ycor() <= -240:
+            new_coords = (self.xcor(), self.ycor())
+        self.goto(new_coords)
+
+
+
+
+
+
+
