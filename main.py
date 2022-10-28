f3cea15590247f06c18e0162a85aa822dcde6c64
TheREALAdr <95942707+TheREALAdr@users.noreply.github.com>
2022-10-27 19:15:41 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+from turtle import Turtle, Screen
+from paddle import Paddle
+
+screenturtle =  Turtle()
+screen = Screen()
+screen.tracer(0)
+
+
+l_paddle = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
+
+game_is_on = True
+
+
+# player_paddle = Paddle(paddle_type="player")
+# ai_paddle = Paddle(paddle_type="ai")
+
+
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("PONGGGG")
