fe34da7541901d7366326057ea0b47d6b90c35d8
TheREALAdr <95942707+TheREALAdr@users.noreply.github.com>
2022-10-27 19:57:48 -0500

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,15 +1,32 @@
-from turtle import Turtle, Screen
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier", 80, "normal")
+
 
 class Scoreboard(Turtle):
+
     def __init__(self):
         super().__init__()
         self.hideturtle()
         self.penup()
-        self.goto(0, 0)
+        self.color("white")
+        self.l_score = 0
+        self.r_score = 0
+        self.update_score()
 
-    def make_line(self):
-        self.pendown()
-        self.backward(10)
-        self.penup()
+    def update_score(self):
+        self.goto(-100, 200)
+        self.write(self.l_score, align=ALIGNMENT, font=FONT)
+        self.goto(100, 200)
+        self.write(self.r_score, align=ALIGNMENT, font=FONT)
 
-   
+    def l_point(self):
+        self.l_score += 1
+        self.clear()
+        self.update_score()
+
+    def r_point(self):
+        self.r_score += 1
+        self.clear()
+        self.update_score()
